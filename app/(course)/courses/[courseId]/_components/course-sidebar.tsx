50bf929a0cbe52b133359899d38a74759cefd07a
Bryson Palmer <brysonpalmer@gmail.com>
2024-09-01 13:22:46 -0700

    New: course id component course sidebar.

--- app/(course)/courses/[courseId]/_components/course-sidebar.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar.tsx
@@ -4,6 +4,8 @@ import { redirect } from 'next/navigation'
 
 import { db } from '@/lib/db'
 
+import { CourseSidebarItem } from './course-sidebar-item'
+
 interface CourseSidebarProps {
   course: Course & {
     chapters: (Chapter & {
@@ -23,9 +25,35 @@ export const CourseSidebar = async ({
     return redirect('/')
   }
 
+  const purchase = await db.purchase.findUnique({
+    where: {
+      userId_courseId: {
+        userId,
+        courseId: course.id
+      }
+    }
+  })
+
   return (
-    <div>
-      Sidebar!
+    <div className='h-full border-r flex flex-col overflow-y-auto shadow-m'>
+      <div className='p-8 flex flex-col border-b'>
+        <h1 className='font-semibold'>
+          {course.title}
+        </h1>
+        {/* Check purchase and add progress */}
+      </div>
+      <div className='flex flex-col w-full'>
+        {course.chapters.map(chapter => (
+          <CourseSidebarItem
+            key={chapter.id}
+            id={chapter.id}
+            label={chapter.title}
+            isCompleted={!!chapter.userProgress?.[0]?.isCompleted}
+            courseId={course.id}
+            isLocked={!chapter.isFree && !purchase}
+          />
+        ))}
+      </div>
     </div>
   )
 } 
