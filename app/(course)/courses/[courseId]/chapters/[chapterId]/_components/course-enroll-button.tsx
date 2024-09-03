9ff7bf14bbe240df8bade15a8c8d41eca8f3d1aa
Bryson Palmer <brysonpalmer@gmail.com>
2024-09-02 19:44:25 -0700

    New: Added course enroll button with client request to server course checkout.

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { useState } from 'react'
+import axios from 'axios'
+
+import { Button } from '@/components/ui/button'
+import { formatPrice } from '@/lib/format'
+import toast from 'react-hot-toast'
+
+interface CourseEnrollButtonProps {
+  courseId: string,
+  price: number
+}
+
+export const CourseEnrollButton = ({
+  courseId,
+  price,
+}: CourseEnrollButtonProps) => {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true)
+      const response = await axios.post(`/api/courses/${courseId}/checkout`)
+
+      window.location.assign(response.data.url)
+    } catch {
+      toast.error('Something went wrong')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return (
+     <Button
+      onClick={onClick}
+      disabled={isLoading}
+      size='sm'
+      className='w-full md:w-auto'
+     >
+      Enroll for {formatPrice(price)}
+     </Button>
+  )
+}
