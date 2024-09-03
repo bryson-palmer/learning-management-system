42d39e7a7ac610b461ae9dcf9442519d5fa703a2
Bryson Palmer <brysonpalmer@gmail.com>
2024-09-03 07:46:09 -0700

    Update: Added course enroll button, progress button description and attachment logic to the course chapter view.

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,10 +1,15 @@
 import { auth } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
+import { File } from 'lucide-react'
 
 import { getChapter } from '@/actions/get-chapter'
 import { Banner } from '@/components/banner'
+import { Separator } from '@/components/ui/separator'
+import { Preview } from '@/components/preview'
 
 import { VideoPlayer } from './_components/video-player'
+import { CourseEnrollButton } from './_components/course-enroll-button'
+import { CourseProgressButton } from './_components/course-progress-button'
 
 const ChapterIdPage = async ({
   params
@@ -63,6 +68,50 @@ const ChapterIdPage = async ({
             isLocked={isLocked}
             completeOnEnd={completeOnEnd}
           />
+        </div>
+        <div>
+          <div className='p-4 flex flex-col md:flex-row items-center justify-between'>
+            <h2 className='text-2xl font-semibold mb-2'>
+              {chapter.title}
+            </h2>
+            {purchase ? (
+              <CourseProgressButton
+                chapterId={params.chapterId}
+                courseId={params.courseId}
+                nextChapterId={nextChapter?.id}
+                isCompleted={!!userProgress?.isCompleted}
+              />
+            ) : (
+              <CourseEnrollButton
+                courseId={params.courseId}
+                price={course.price!}
+              />
+            )}
+          </div>
+          <Separator />
+          <div>
+            <Preview value={chapter.description!} />
+          </div>
+          {!!attachments.length && (
+            <>
+              <Separator />
+              <div className='p-4'>
+                {attachments.map(attachment => (
+                  <a
+                    key={attachment.id}
+                    href={attachment.url}
+                    target='_blank'
+                    className='flex items-center p-3 w-full bg-sky-200 border text-sky-700 rounded-md hover:underline'
+                  >
+                    <File />
+                    <p className='line-clamp-1'>
+                      {attachment.name}
+                    </p>
+                  </a>
+                ))}
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
