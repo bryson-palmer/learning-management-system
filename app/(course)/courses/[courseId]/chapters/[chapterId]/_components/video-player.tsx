1590fe4b56a6d2cf65e69d743a152106b2b606a7
Bryson Palmer <brysonpalmer@gmail.com>
2024-09-03 08:07:33 -0700

    Update: Added onEnd functionality to video player component.

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -30,6 +30,31 @@ export const VideoPlayer = ({
   completeOnEnd
 }: VideoPlayerProps) => {
   const [isReady, setIsReady] = useState(false)
+  const router = useRouter()
+  const confetti = useConfettiStore()
+
+  const onEnd = async () => {
+    try {
+      if (completeOnEnd) {
+        await axios.put(`/api/courses/${courseId}/chapters/${chapterId}/progress`, {
+          isCompleted: true,
+        })
+
+        if (!nextChapterId) {
+          confetti.onOpen()
+        }
+        
+        toast.success('Progress updated')
+        router.refresh()
+
+        if (nextChapterId) {
+          router.push(`/courses/${courseId}/chapters/${nextChapterId}`)
+        }
+      }
+    } catch {
+      toast.error('Something went wrong')
+    }
+  }
 
   return (
     <div className='relative aspect-video'>
@@ -54,7 +79,7 @@ export const VideoPlayer = ({
             !isReady && 'hidden'
           )}
           onCanPlay={() => setIsReady(true)}
-          onEnded={() => {}}
+          onEnded={onEnd}
           playbackId={playbackId}
         />
       )}
