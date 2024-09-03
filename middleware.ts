0e8dce13366638ffeea3e582a182e917aa661d9d
Bryson Palmer <brysonpalmer@gmail.com>
2024-09-03 09:00:51 -0700

    Update: Added upload thing route to public routes in middleWare.

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/webhook'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/webhook', '/api/uploadthing'])
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
