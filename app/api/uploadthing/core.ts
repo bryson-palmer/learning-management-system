84f09c6b9022dc0980fc7999ce45b1cbcc5e8524
Bryson Palmer <brysonpalmer@gmail.com>
2024-09-07 17:32:52 -0700

    Update: Added isTeacher util to uplaod thing api to protect route.

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,11 +1,15 @@
-import { auth } from "@clerk/nextjs/server";
-import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { auth } from '@clerk/nextjs/server'
+import { createUploadthing, type FileRouter } from 'uploadthing/next'
+
+import { isTeacher } from '@/lib/teacher'
  
-const f = createUploadthing();
+const f = createUploadthing()
  
 const handleAuth = () => {
   const { userId } = auth()
-  if (!userId) throw new Error('Unauthorized')
+  const isAuthorized = isTeacher(userId)
+
+  if (!userId || !isAuthorized) throw new Error('Unauthorized')
   return { userId }
 }
  
@@ -20,43 +24,43 @@ export const ourFileRouter = {
   chapterVideo: f({ video: { maxFileSize: '512GB', maxFileCount: 1 }})
     .middleware(() => handleAuth())
     .onUploadComplete(() => {})
-} satisfies FileRouter;
+} satisfies FileRouter
  
-export type OurFileRouter = typeof ourFileRouter;
+export type OurFileRouter = typeof ourFileRouter
 
 /*
-  import { createUploadthing, type FileRouter } from "uploadthing/next";
-  import { UploadThingError } from "uploadthing/server";
+  import { createUploadthing, type FileRouter } from 'uploadthing/next'
+  import { UploadThingError } from 'uploadthing/server'
   
-  const f = createUploadthing();
+  const f = createUploadthing()
   
-  const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
+  const auth = (req: Request) => ({ id: 'fakeId' }) // Fake auth function
   
   // FileRouter for your app, can contain multiple FileRoutes
   export const ourFileRouter = {
     // Define as many FileRoutes as you like, each with a unique routeSlug
-    imageUploader: f({ image: { maxFileSize: "4MB" } })
+    imageUploader: f({ image: { maxFileSize: '4MB' } })
       // Set permissions and file types for this FileRoute
       .middleware(async ({ req }) => {
         // This code runs on your server before upload
-        const user = await auth(req);
+        const user = await auth(req)
   
         // If you throw, the user will not be able to upload
-        if (!user) throw new UploadThingError("Unauthorized");
+        if (!user) throw new UploadThingError('Unauthorized')
   
         // Whatever is returned here is accessible in onUploadComplete as `metadata`
-        return { userId: user.id };
+        return { userId: user.id }
       })
       .onUploadComplete(async ({ metadata, file }) => {
         // This code RUNS ON YOUR SERVER after upload
-        console.log("Upload complete for userId:", metadata.userId);
+        console.log('Upload complete for userId:', metadata.userId)
   
-        console.log("file url", file.url);
+        console.log('file url', file.url)
   
         // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-        return { uploadedBy: metadata.userId };
+        return { uploadedBy: metadata.userId }
       }),
-  } satisfies FileRouter;
+  } satisfies FileRouter
   
-  export type OurFileRouter = typeof ourFileRouter;
+  export type OurFileRouter = typeof ourFileRouter
 */
