8471f675e2eccadfd0c903a3c3b7d4f3de39cf6c
Bryson Palmer <brysonpalmer@gmail.com>
2024-09-07 17:17:16 -0700

    Update: Added chart and data cards to teacher analytics page.

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,7 +1,40 @@
-const AnalyticsPage = () => {
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+
+import { getAnalytics } from '@/actions/get-analytics'
+
+import { Chart } from './_components/chart'
+import { DataCard } from './_components/data-card'
+
+const AnalyticsPage = async () => {
+  const { userId } = auth()
+
+  if (!userId) {
+    return redirect('/')
+  }
+
+  const {
+    data,
+    totalRevenue,
+    totalSales,
+  } = await getAnalytics(userId)
+
   return (
-    <div>
-      Analytics page!
+    <div className='p-6'>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-4 mb-4'>
+        <DataCard
+          shouldFormat
+          value={totalRevenue}
+          label='Total Revenue'
+        />
+        <DataCard
+          value={totalSales}
+          label='Total Sales'
+        />
+      </div>
+      <Chart
+        data={data}
+      />
     </div>
   );
 }
