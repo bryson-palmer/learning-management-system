fcf50805e05d5fadff8a1c961778f9022d8a6297
Bryson Palmer <brysonpalmer@gmail.com>
2024-09-08 19:04:13 -0700

    Update: Adding debug true in middleware to help me debug vercel deployment issue.

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,14 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/webhook', '/api/uploadthing'])
 
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect()
-  }
-})
+export default clerkMiddleware(
+  (auth, request) => {
+    if (!isPublicRoute(request)) {
+      auth().protect()
+    }
+  },
+  { debug: true }
+)
 
 export const config = {
   matcher: [
