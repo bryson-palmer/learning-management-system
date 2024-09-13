e6067d0390da96f9ff96d1497f4c637b60bb8b08
Bryson Palmer <brysonpalmer@gmail.com>
2024-09-13 14:52:56 -0700

    Update: course progress button background.

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-progress-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-progress-button.tsx
@@ -59,7 +59,7 @@ export const CourseProgressButton = ({
       onClick={onClick}
       disabled={isLoading}
       type='button'
-      variant={isCompleted ? 'outline' : 'success'}
+      variant={isCompleted ? 'default' : 'success'}
       className='w-full md:w-auto'
     >
       {isCompleted ? 'Undo completed' : 'Mark as complete'}
