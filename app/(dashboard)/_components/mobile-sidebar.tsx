5a5375b65db1c8d62f34173eca2c54da957de90d
Bryson Palmer <brysonpalmer@gmail.com>
2024-09-13 10:14:35 -0700

    Wrapping radix VissuallyHiddent around shadcn sheet components needed for screen reader compliance.

--- app/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -2,8 +2,12 @@ import { Menu } from 'lucide-react'
 import {
   Sheet,
   SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
   SheetTrigger
 } from '@/components/ui/sheet'
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden"
 import { Sidebar } from './sidebar'
 
 export const MobileSidebar = () => {
@@ -13,7 +17,13 @@ export const MobileSidebar = () => {
         <Menu />
       </SheetTrigger>
       <SheetContent side='left' className='p-0 bg-white'>
-        <Sidebar />
+        {/* Added hidden sheet header to stay complient with screen readers */}
+        <VisuallyHidden.Root>
+          <SheetHeader>
+            <SheetTitle>Dialog Content</SheetTitle>
+            <SheetDescription>Menu drawer for mobile screens.</SheetDescription>
+          </SheetHeader>
+        </VisuallyHidden.Root>
       </SheetContent>
     </Sheet>
   )
