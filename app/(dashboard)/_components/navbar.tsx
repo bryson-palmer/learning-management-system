924c57573dd2ced887ac249b24fd5e9acd7c2307
Bryson Palmer <brysonpalmer@gmail.com>
2024-09-13 21:38:31 -0700

    Update: Moved app mobile sidebar into navbar.

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,10 +1,14 @@
-import { NavbarRoutes } from "@/components/navbar-routes"
-import { MobileSidebar } from "./mobile-sidebar"
+import { NavbarRoutes } from '@/components/navbar-routes'
+import { MobileDrawer } from '@/components/mobile-drawer'
+
+import { Sidebar } from './sidebar'
 
 export const Navbar = () => {
   return (
     <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
-      <MobileSidebar />
+      <MobileDrawer>
+        <Sidebar />
+      </MobileDrawer>
       <NavbarRoutes />
     </div>
   )
